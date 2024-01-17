0de665db0fa0186abe677614d062486ed3ab4fd0
Stanislaw Mazowiecki <stanislaw.mazowiecki@silksh.pl>
2024-01-17 14:52:10 +0100

    fix: Fix ci
    Signed-off-by: Stanislaw Mazowiecki <stanislaw.mazowiecki@arianelabs.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,10 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+      files: ['*.ts'],
     },
   ],
+  ignorePatterns: ['node_modules/', 'dist/'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
