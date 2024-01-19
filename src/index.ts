67e34d3765e67b3f6d38bf166e9554aa755e10b4
Michal Rozek <michal.rozek@arianelabs.com>
2024-01-19 16:10:20 +0100

    feat: fetchForTesting func with e2e test added
    Signed-off-by: Michal Rozek <michal.rozek@arianelabs.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import axios from 'axios';
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('dev only output');
   }
   return a + b;
+};
+
+// TODO: function fetchForTesting needs to be deleted after we agree that our testing flow is correct
+export const fetchForTesting = async () => {
+  const API_URL = 'https://testnet.mirrornode.hedera.com/api/v1/accounts';
+  const response = await axios.get(API_URL);
+  return response;
 };
