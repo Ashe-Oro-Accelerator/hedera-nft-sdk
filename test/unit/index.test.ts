b188a634e0accfcd8961d6ab8d5ce94236e46517
Michal Rozek <michal.rozek@arianelabs.com>
2024-01-23 16:57:35 +0100

    fix: env from test removed
    Signed-off-by: Michal Rozek <michal.rozek@arianelabs.com>

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -1,8 +1,5 @@
-import 'dotenv/config';
-
 describe('sum', () => {
-  const testValue = Number(process.env.TEST_VALUE);
   it('adds two numbers together', () => {
-    expect(1 + 1).toEqual(testValue);
+    expect(1 + 1).toEqual(2);
   });
 });
