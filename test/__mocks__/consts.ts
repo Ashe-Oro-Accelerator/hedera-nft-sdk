87744331a220818f57360c459cf1b72e0afaf3be
Stanislaw Mazowiecki <stanislaw.mazowiecki@arianelabs.com>
2024-02-07 11:10:00 +0100

    Fix: Pr fix
    Signed-off-by: Stanislaw Mazowiecki <stanislaw.mazowiecki@arianelabs.com>

--- test/__mocks__/consts.ts
+++ test/__mocks__/consts.ts
@@ -2,8 +2,8 @@ export const myAccountId = '0.0.12345';
 export const myPrivateKey =
   '123e020100300506032b657004220420f8e9f8de8f7e06f7e9f8de8f7e06f7e9f8de8f7e06f7e9f8de8f7e06f7e9f8de';
 
-export const pathToOneLineCSV = 'test/__mocks__/testRows.csv';
-export const pathToRowCSV = 'test/__mocks__/testRows.csv';
+export const pathToOneLineCSV = 'test/__mocks__/csv/csvExampleOneLine.csv';
+export const pathToRowCSV = 'test/__mocks__/csv/csvExampleRows.csv';
 export const CSV_EXAMPLE_INVALID_HEADERS = 'test/__mocks__/csv/csvExampleInvalidHeaders.csv';
 export const CSV_EXAMPLE_NO_IMAGES = 'test/__mocks__/csv/csvExampleNoImages.csv';
 export const CSV_EXAMPLE_ONLY_REQUIRED_FIELDS =
