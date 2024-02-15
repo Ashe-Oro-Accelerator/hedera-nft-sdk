8930d2d48dfec0dd60f25cf39931c335a62d18d4
michalrozekariane <michal.rozek@arianelabs.com>
2024-02-15 11:04:24 +0100

    fix: ipfs link formatting
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/utils/helpers/nftMetadataDecoder.ts
+++ src/utils/helpers/nftMetadataDecoder.ts
@@ -4,22 +4,30 @@ import { errorToMessage } from './errorToMessage';
 
 export const nftMetadataDecoder = (nfts: NFTDetails[], ipfsGateway?: string): DecodedMetadata[] => {
   const decodedMetadataArray: DecodedMetadata[] = nfts.map((nft: NFTDetails) => {
-    // console.log('metadata BEFORE atob', nft.metadata);
     let decodedNFTMetadata: string;
 
     try {
       decodedNFTMetadata = atob(nft.metadata);
-      // console.log('metadata AFTER atob:', decodedNFTMetadata);
     } catch (error) {
       throw new Error(errorToMessage(error));
     }
 
+    if (
+      !decodedNFTMetadata.startsWith('https://') &&
+      !decodedNFTMetadata.startsWith('http://') &&
+      !ipfsGateway
+    ) {
+      throw new Error(dictionary.errors.ipfsGatewayRequired);
+    }
+
     if (decodedNFTMetadata.startsWith('ipfs://') && ipfsGateway) {
       decodedNFTMetadata = decodedNFTMetadata.replace('ipfs://', ipfsGateway);
-    } else if (!decodedNFTMetadata.startsWith('ipfs://') && ipfsGateway) {
+    } else if (
+      !decodedNFTMetadata.startsWith('https://') &&
+      !decodedNFTMetadata.startsWith('http://') &&
+      ipfsGateway
+    ) {
       decodedNFTMetadata = `${ipfsGateway}${decodedNFTMetadata}`;
-    } else if (!ipfsGateway && decodedNFTMetadata.startsWith('ipfs://')) {
-      throw new Error(dictionary.errors.ipfsGatewayRequired);
     }
 
     return {
