1c8b3e29ff8f29c1d3b731378f45a2d916692f7c
michalrozekariane <michal.rozek@arianelabs.com>
2024-02-16 09:03:38 +0100

    feat: add e2e happy path test for createAndValidateSharedNFTCollectionMetadata
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- test/e2e/e2eConsts.ts
+++ test/e2e/e2eConsts.ts
@@ -11,3 +11,15 @@ export const secondPrivateKey = process.env.SECOND_PRIVATE_KEY!;
 export const nftSDK = new HederaNFTSDK(operatorAccountId, operatorPrivateKey, 'testnet');
 
 export const feeFactoryInstance = new FeeFactory();
+
+export const NETWORK = 'testnet';
+export const IPFS_GATEWAY = 'https://cloudflare-ipfs.com/ipfs/';
+
+export const LINK_TO_JSON_OBJECT_WITHOUT_ERRORS =
+  'https://violet-written-whale-308.mypinata.cloud/ipfs/QmVbS1sbVWKe13RYUiwPkzG5cCESM2NHTx3CuhssYUvcRn';
+
+export const LINK_TO_JSON_OBJECT_WITH_MISSING_FIELDS =
+  'https://violet-written-whale-308.mypinata.cloud/ipfs/QmWC8VUgSkPM62mBznXqwqSvsCMSad1bxDzzqcwwYWueuf';
+
+export const AMOUNT_OF_NFTS_TO_MINT = 35;
+export const BATCH_SIZE = 5;
